424765d91da97933a8ae55bb3512e705ea78e3cd
llop <albertlobo1981@yahoo.es>
2016-05-09 17:00:18 +0200

    unity

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+
+	public Button playButton;
+	public Button quitButton;
+
+	// Use this for initialization
+	void Start () {
+		playButton = playButton.GetComponent<Button>();
+		quitButton = quitButton.GetComponent<Button>();
+	}
+
+	public void playClicked() {
+		SceneManager.LoadScene("Scenes/Scene01");
+	}
+
+	public void quitClicked() {
+		Application.Quit();
+	}
+
+}
